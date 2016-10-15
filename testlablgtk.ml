6e8ec235e56a10efbcf12fdadc82ce9a23f0cf23
Nounours <le-run@crans.org>
2016-10-15 11:48:34 +0200

    Random gui tests

--- testlablgtk.ml
+++ testlablgtk.ml
@@ -4,7 +4,7 @@ open GdkKeysyms
 let locale = GtkMain.Main.init ()
 
 let main () =
-    let window = GWindow.window ~width:320 ~height:240
+    let window = GWindow.window ~resizable:true ~width:320 ~height:240
                                     ~title:"Simple lablgtk program" () in
     let vbox = GPack.vbox ~packing:window#add () in
     window#connect#destroy ~callback:Main.quit;
@@ -14,10 +14,15 @@ let main () =
     let factory = new GMenu.factory menubar in
     let accel_group = factory#accel_group in
     let file_menu = factory#add_submenu "File" in
+    let test_menu = factory#add_submenu "Test" in
 
     (* File menu *)
     let factory = new GMenu.factory file_menu ~accel_group in
     factory#add_item "Quit" ~key:_Q ~callback: Main.quit;
+
+    (* Test menu *)
+    let factory = new GMenu.factory test_menu ~accel_group in
+    factory#add_item "Hello" ~key:_H ~callback: (fun () -> Printf.printf "Hello");
 
     (* Button *)
     let button = GButton.button ~label:"Push me!"
