2a7dd0650f920a563c51e0cbf6610dd121f64d29
141592653 <julien.rixe@laposte.net>
2016-11-09 19:06:34 +0100

    beginning of the conversion between ast and top level ast

--- src/ide.ml
+++ src/ide.ml
@@ -1,6 +1,8 @@
-
 open Gui
+open Tl_ast
 
 let () =
-    Printf.printf "%s\n" "*********************** IDE ***********************";
-    gui ()
+  let tl = get_ast "src/ide.ml" and s = file_to_string "src/ide.ml" in 
+  (*print_ast tl;*)
+  print_tl_ast (ast_to_tl_ast s tl);
+  gui ()
