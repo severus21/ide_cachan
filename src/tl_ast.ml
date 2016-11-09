025044855a6f212ca3ff6a99a552e091ddffd8cb
141592653 <julien.rixe@laposte.net>
2016-11-09 19:30:35 +0100

    Restructuration, added some comments

--- src/tl_ast.ml
+++ src/tl_ast.ml
@@ -1,11 +1,12 @@
 open Parsetree
 
+(** top-level structures*)
 type tl_struct =  
-|Tl_none (*cette valeur doit être supprimée*)
-|Tl_open of string*string (*pour le moment, on se contente d'extraire le nom du module ouvert
-			   *et on donne également la ligne complète de l'ouverture*)
+|Tl_none (*to be removed*)
+|Tl_open of string*string (*for the moment , we only extract the name of the module opened
+			   *and we give the complete line of the openning*)
 
-(** Type des ast top level*)
+(** Top-level ast type*)
 type tl_ast = tl_struct list
 
 
@@ -24,10 +25,18 @@ let get_ast f =
   Parse.implementation (Lexing.from_channel (open_in f)) 
 
 
-  
-
 let print_ast ast = 
   Printast.implementation (Format.formatter_of_out_channel stdout) ast
+
+
+
+(* ***BEGIN Miscallaneous functions on locations*)
+
+
+(* ***END Miscallaneous functions on locations*)
+
+
+(* ***BEGIN Conversion from ast to tl_ast*)
 
 
 let struct_to_tl_struct ml  = function {pstr_desc = struct_item; pstr_loc = _} ->
@@ -38,12 +47,20 @@ let struct_to_tl_struct ml  = function {pstr_desc = struct_item; pstr_loc = _} -
     |_ -> Tl_none
   end
 
-(*ml est la chaîne de charactère contenant le fichier entier dont provient ast *)
+(*ml is a string containing the whole file from which ast was created *)
 let ast_to_tl_ast ml ast = List.map (struct_to_tl_struct ml) ast
+
+
+(* ***END Conversion from ast to tl_ast*)
   
+
+(* ***BEGIN Printing of a tl_ast*)
 let print_tl_struct tl_s = match tl_s with 
   |Tl_open(s1,s2)-> Printf.printf "%s %s\n" s1 s2
   |_ -> ()
     
 
 let print_tl_ast tl = List.fold_left (fun () -> print_tl_struct) () tl
+
+
+(* ***END Printing of a tl_ast*)
--- src/tl_ast.mli
+++ src/tl_ast.mli
@@ -1,31 +1,29 @@
-(** Ce module contient des fonctions permettant de manipuler des ast top_level de ocaml*)
+(** This module contains functions allowing to manipulate ocaml top-level ast*)
 
-
-
-
+(** top-level structures*)
 type tl_struct =  
-|Tl_none (*cette valeur doit être supprimée*)
-|Tl_open of string*string (*pour le moment, on se contente d'extraire le nom du module ouvert
-			   *et on donne également la ligne complète de l'ouverture*)
+|Tl_none (*to be removed*)
+|Tl_open of string*string (*for the moment , we only extract the name of the module opened
+			   *and we give the complete line of the openning*)
 
-(** Type des ast top level*)
+(** Top-level ast type*)
 type tl_ast = tl_struct list
 
 
-(** Renvoie la chaîne de charactère contenant le fichier f complet *)
+(** Returns the string containing the whole file f *)
 val file_to_string : string -> string
 
-(** Permet d'obtenir un parsetree du fichier ocaml dont le nom est donné en argument*)
+(** Gets a parsetree of the ocaml file given in argument *)
 val get_ast : string ->  Parsetree.structure_item list
 
-(** Affiche un parsetree*)
+(** Prints a parstree *)
 val print_ast : Parsetree.structure_item list -> unit
 
 
-(** Conversion des ast de Parsetree vers les tl_ast *)
+(** Converts a parsetree into a tl_ast *)
 val ast_to_tl_ast : string -> Parsetree.structure_item list -> tl_ast
 
-(** Affiche un tl_ast *)
+(** Prints a tl_ast *)
 val print_tl_ast : tl_ast -> unit
 
 
