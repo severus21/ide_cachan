5beccf16ca9983c37e68691723747d5e710edbf9
Kevin Le run <le-run@crans.org>
2016-11-10 11:49:10 +0100

    [gui] la navlist contiens et affiche des gset et non plus des string

--- src/guiNavlist.ml
+++ src/guiNavlist.ml
@@ -1,25 +1,32 @@
 open Gobject.Data
+open Coeur
 
 class navlist ~packing =
     let hbox = GPack.hbox ~packing () in
     let make_view model column =
         let view = GTree.view ~model ~packing:hbox#add () in
-        let renderer = GTree.cell_renderer_text [], ["text", column] in
-        let viewcol = GTree.view_column ~renderer () in
+        let renderer = GTree.cell_renderer_text [] in
+        let cell_data_func (model:GTree.model) iter =
+            let set:gset = model#get ~row:iter ~column in
+            let name = set#name in
+            renderer#set_properties [`TEXT name]
+        in
+        let viewcol = GTree.view_column ~renderer:(renderer, []) () in
+        viewcol#set_cell_data_func renderer cell_data_func;
         ignore(view#append_column viewcol);
         view
     in
     let _cols = new GTree.column_list in
-    let _col1 = _cols#add string
-    and _col2 = _cols#add string
-    and _col3 = _cols#add string in
+    let _col1 = _cols#add caml
+    and _col2 = _cols#add caml
+    and _col3 = _cols#add caml in
     let _model1 = GTree.list_store _cols
     and _model2 = GTree.list_store _cols
     and _model3 = GTree.list_store _cols in
     let _view1 = make_view _model1 _col1
     and _view2 = make_view _model2 _col2
     and _view3 = make_view _model3 _col3 in
-    object
+    object(self)
         val cols = _cols
         val col1 = _col1
         val col2 = _col2
@@ -35,7 +42,7 @@ class navlist ~packing =
         method set_data ~col ~data () =
             let _set_data (model:GTree.list_store) column =
                 model#clear ();
-                let fill (value:string) =
+                let fill (value:gset) =
                     let iter = model#append () in
                     model#set ~row:iter ~column:column value
                 in
@@ -55,5 +62,7 @@ class navlist ~packing =
             | 3 -> (view3#get_column 0)#set_title title
             | _ -> failwith (Printf.sprintf "No such column (%i)" col)
 
+        initializer
+            self#set_data ~col:1 ~data:[new set("test")] ()
     end
 ;;
