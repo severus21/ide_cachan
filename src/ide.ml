3649db2965ed4e60f2f6a7017662cd544aec3158
Alice Rixte <alice.rixte@gmail.com>
2016-11-15 20:41:59 +0100

    added unit tests

--- src/ide.ml
+++ src/ide.ml
@@ -2,6 +2,8 @@ open Tl_ast
 
 
 let () =
-  let tl = get_ast "src/test_ast.ml" and s = file_to_string "src/test_ast.ml" in 
+  (*let tl = get_ast "src/test_ast.ml" and s = file_to_string "src/test_ast.ml" in 
+  
   print_ast tl;
-  print_tl_ast (ast_to_tl_ast s tl);
+  print_tl_ast (ast_to_tl_ast s tl);*)
+  unit_tests ()
