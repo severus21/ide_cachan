ca2819298ee934521709ee7540d0627f52882cf8
Alice Rixte <alice.rixte@gmail.com>
2016-11-16 20:37:33 +0100

    Dependencies tag so that Gabriel can compute the dependency graph

--- src/plugin_fe_ml.ml
+++ src/plugin_fe_ml.ml
@@ -1,16 +1,36 @@
 open Core
+open Tl_ast
 
 module Plugin_ml : Plugin_fe.Plugin = struct
   let file_extensions  = [["ml";"mli"];["ml"]]
 
-  (*let tl_struct_to_set tl_str = match tl_str with 
-    |Tl_none -> new set("blub")
-    |Tl_open(open_ls,lign) -> 
-      let s = new set("")
-    |Tl_var(_,_) ->
-    |Tl_fun(_,_) ->
-    |Tl_exception(_,_) ->
-    |Tl_type(_,_) -> *)
+  let tl_struct_to_set struc (father:gset) = match struc with 
+    |Tl_none -> failwith "obsolete"
+    |Tl_open(open_ls,lign) -> ignore lign;
+      begin
+	match father#meta_data_sys#get_value "dependencies" with 
+	|Some values -> father#meta_data_sys#add_tag "dependencies" ((TDepend open_ls)::values)
+	|None -> father#meta_data_sys#add_tag "dependencies" [TDepend open_ls] 
+      end
+    |Tl_var(_,_) -> failwith "not implemented"
+    |Tl_fun(_,_) -> failwith "not implemented"
+    |Tl_exception(_,_) -> failwith "not implemented"
+    |Tl_type(_,_) -> failwith "not implemented"
+
+  (** "blub.ml" -> "Blub" *)
+  let file_name_to_module_name f = 
+    String.uppercase_ascii (String.sub f 0 1) ^ (String.sub f 1 (String.length f - 3))
+
+  let tl_ast_to_set ast file_name= 
+    let s: gset  = new set(file_name_to_module_name file_name) in 
+    let rec browse_ast ast' = match ast' with
+      |[] -> ()
+      |struc::q -> tl_struct_to_set struc s; 
+	browse_ast q
+    in
+    browse_ast ast;
+    s
+
 
   let string_to_set ext files = 
     if List.length ext <> List.length files then
@@ -20,12 +40,11 @@ module Plugin_ml : Plugin_fe.Plugin = struct
 	match ext with
 	|["ml"] ->
 	  begin 
-	    let ml_str = List.hd files in 
-	    let ast = Tl_ast.ast_to_tl_ast ml_str in 
-	    ignore ast;
-	    new set("blub")
+	    let file_name = List.hd files in 
+	    let ast = ast_to_tl_ast (file_to_string file_name) in  
+	    tl_ast_to_set ast file_name
 	  end
-	|["ml";"mli"] -> new set("blub")
+	|["ml";"mli"] -> failwith "not implemented"
 	| _ -> failwith "The plugin ml cannot import these file extensions (plugin_fe_ml.ml)" 
       end
 
