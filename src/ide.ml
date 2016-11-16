e4e934c155c39e7368ed3f6fc0326b23b70e4f9d
141592653 <julien.rixe@laposte.net>
2016-11-16 17:42:04 +0100

    Repair main

--- src/ide.ml
+++ src/ide.ml
@@ -1,10 +1,7 @@
-open Tl_ast
-open Plugin_fe_ml
+
+
+open Gui
 
 let () =
-  (*let tl = get_ast "src/test_ast.ml" and s = file_to_string "src/test_ast.ml" in 
-  
-  print_ast tl;
-  print_tl_ast (ast_to_tl_ast s tl);*)
-  ignore (Plugin_ml.file_extensions);
-  unit_tests ()
+    Printf.printf "%s\n" "*********************** IDE ***********************";
+gui ()
