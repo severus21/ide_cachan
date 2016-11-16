3c1ba9279b60d13ccd8d9884df459c30bb25c4d9
Alice Rixte <alice.rixte@gmail.com>
2016-11-16 16:17:02 +0100

    skeleton for the front-end of the ocaml plugin

--- src/ide.ml
+++ src/ide.ml
@@ -1,8 +1,10 @@
 open Tl_ast
+open Plugin_fe_ml
 
 let () =
   (*let tl = get_ast "src/test_ast.ml" and s = file_to_string "src/test_ast.ml" in 
   
   print_ast tl;
   print_tl_ast (ast_to_tl_ast s tl);*)
+  ignore (Plugin_ml.file_extensions);
   unit_tests ()
