fabf54d97dd0d55d14e25304c2b6da1d642402dd
Laurent Prosperi <laurent.prosperi@ens-cachan.fr>
2016-11-16 12:34:24 +0100

    added unit tests

--- src/tl_ast.ml
+++ src/tl_ast.ml
@@ -10,7 +10,7 @@ type tl_struct =
 |Tl_var of string * string
 |Tl_fun of string * string
 |Tl_exception of string * string
-|Tl_type of (string * string) list
+|Tl_type of string list * string
 
 (** Top-level ast type*)
 type tl_ast = tl_struct list
@@ -62,20 +62,17 @@ let struct_to_tl_struct ml  = function {pstr_desc = struct_item; pstr_loc = loc}
 				     get_str_from_location ml loc)
     |Pstr_value(_,  value::_)->((*pour l'instant on ne traite que la première*)
         match value.pvb_pat.ppat_desc  with 
-        |Ppat_var loc->(
-            let name = loc.txt and expr = (get_str_from_location ml 
-                                             value.pvb_expr.pexp_loc) in
+        |Ppat_var loc_var->(
+            let name = loc_var.txt and expr = (get_str_from_location ml loc) in
             match value.pvb_expr.pexp_desc with
             | Pexp_function _ | Pexp_fun _-> Tl_fun(name, expr)   
             | _ -> Tl_var(name, expr)   
         )(*comment faire avec les autrs patterns???*)
         |_->Tl_none                  
     )
-    |Pstr_exception e-> Tl_exception( e.pext_name.txt, get_str_from_location ml e.pext_loc )
-    |Pstr_type(_,decls)->Tl_type( List.map( 
-        function decl ->
-        (decl.ptype_name.txt, get_str_from_location ml decl.ptype_name.loc)
-       ) decls )
+    |Pstr_exception e->Tl_exception( e.pext_name.txt, get_str_from_location ml loc)
+    |Pstr_type(_,decls)->Tl_type((List.map (function d->d.ptype_name.txt) decls), 
+       get_str_from_location ml loc)
     |_ -> Tl_none
   end
 
@@ -94,8 +91,9 @@ let tl_struct_to_str tl_s = match tl_s with
     |Tl_open(_,s)-> Format.sprintf "%s\n" s
     |Tl_var(name, expr)->Format.sprintf "let %s=%s\n" name expr 
     |Tl_fun(name, expr)->Format.sprintf "letfun %s %s\n" name expr
-    |Tl_exception(name, values)->Format.sprintf "exception %s : %s" name values        |Tl_type values->(
-        List.fold_left (fun head (name,value)-> Format.sprintf "%s type %s : %s\n" head name value) "" values   
+    |Tl_exception(name, values)->Format.sprintf "exception %s : %s" name values
+    |Tl_type(names, value)->(
+        List.fold_left (fun head name-> Format.sprintf "%s type %s : %s\n" head name value) "" names   
     )
     |_ -> ""
 
@@ -121,8 +119,35 @@ let test_open _ =
   assert_equal (quick_tl_struct "open Module1.Module2") 
     (Tl_open(["Module1";"Module2"],"open Module1.Module2"))
 
+let test_var _ =
+  assert_equal (quick_tl_struct "let a = 3")
+    (Tl_var("a","let a = 3"))
+
+let test_var_ref _ =
+  assert_equal (quick_tl_struct "let a = ref [\"test\"]")
+    (Tl_var("a", "let a = ref [\"test\"]"))
+
+let test_fun _ =
+  assert_equal (quick_tl_struct "let f x = x")
+    (Tl_fun("f", "let f x = x"))
+
+let test_fun_function _ = 
+  assert_equal (quick_tl_struct "let f = function x -> x")
+    (Tl_fun("f", "let f = function x -> x"))
+
+let test_fun_fun _ = 
+  assert_equal (quick_tl_struct "let f = fun x y-> x+y")
+    (Tl_fun("f", "let f = fun x y-> x+y"))
+
+let test_exception _ =
+  assert_equal (quick_tl_struct "exception E of string")
+    (Tl_exception("E", "exception E of string"))
+
 let test_structs = 
   "struct tests">:::
-    ["open">::test_open]
+    ["open">::test_open; "var">::test_var; "var_ref">::test_var_ref;
+    "fun">::test_fun; "test_fun_function">::test_fun_function;
+    "fun_function">::test_fun_function; "fun_fun">:: test_fun_fun;
+    "exception">::test_exception]
 
 let unit_tests () = run_test_tt_main test_structs
