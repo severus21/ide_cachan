01e7d9ab2f261c3c43a1dc9a34622825e0d29416
Boudy <boudy@skynet>
2016-11-16 21:00:53 +0100

    les noeuds du graphe de dépendance s'affichent

--- src/dep_graph_build.ml
+++ src/dep_graph_build.ml
@@ -4,7 +4,7 @@
 
 (** Définitions de variables globales **)
 
-let fic_out = open_out "./graph.tex"
+let open_fic_out () = open_out "./graph.tex"
 
 let header = "
 \\documentclass{article}
@@ -31,33 +31,44 @@ let ender = "
 
 (** Fonctions utiles **)
 
-(*let make_env ..........*)
-
 (* cette fonction va chercher (où ??) la liste des open faits dans le module en question *)
 let get_open nom_du_module = [[nom_du_module^"_Iterables";nom_du_module^"_Listes"];["Toto"]]
 
-(*let make_nodes liste size =*)
+(** Construction des noeuds **)
+
+(* renvoie la position du module de numéro index, sur un cercle de rayon 70 et d'origine 0,-100 *)
+let give_one_pos size index =
+	let angle = 8. *. atan(1.) *. (float_of_int index) /. (float_of_int size) in
+	let abscisse = floor (70. *. (cos angle)) in
+	let ordonnee = floor (70. *. (sin angle) -. 100.) in
+	(int_of_float abscisse , int_of_float ordonnee)
+
+(* renvoie la liste des positions correspondant à un graphe de dépendances à n modules *)
+let rec give_positions size acc = match acc with
+	|0 -> []
+	|_ -> (give_one_pos size acc)::(give_positions size (acc-1))
+
+(* affiche la définition des noeuds dans le .tex *)
+let rec print_nodes modules liste_coord out = match liste_coord with
+  |[] -> ()
+  |(x,y)::q -> (let modul = List.hd modules in
+    output_string out ("  \\node(" ^ modul ^ ")(" ^ (string_of_int x) ^ "," ^ (string_of_int y) ^ "){" ^ modul ^ "}\n") ;
+    print_nodes (List.tl modules) q out)
+
+(* affiche la partie 'définitions des noeuds' en entier *)
+let print_nodes_complete modules out =
+  output_string out "  \\gasset{Nadjust=w,Nadjustdist=2,Nh=6,Nmr=1}\n" ;
+  let size = List.length modules in
+	let liste_coord = give_positions size size in
+	print_nodes modules liste_coord out
+
+
 	
 
 
-(* main est appelée par GUI
-   liste est la liste des modules pour lesquels l'utilisateur veut des infos
+(* build_graph est appelée par GUI
+   modules est la liste des modules pour lesquels l'utilisateur veut des infos
    flag indique si l'utilisateur veut avoir des informations entre les éléments de liste, ou savoir TOUT sur chaque élément de liste (qui l'ouvre par exemple) (pour plus tard) *)
-   
-let boudy_main liste flag = if liste = [] && flag = 0 then output_string fic_out "
-  \\gasset{Nadjust=w,Nadjustdist=2,Nh=6,Nmr=1}
-  \\node[Nmarks=i](A)(0,0){idle}
-  \\node(B)(50,0){wait}
-  \\node(C)(50,-20){wait}
-  \\node[Nmarks=r](D)(0,-20){critical}
- 
-  \\drawedge(A,B){req1:=true}
-  \\drawedge(B,C){turn:=2}
-  \\drawedge[syo=-1,eyo=-1](C,D){turn=1?}
-  \\drawedge[syo=1,eyo=1,ELside=r](C,D){req556666652=false?}
-  \\drawedge(D,A){req1:=false}
-"
-
 
 
 
@@ -67,20 +78,30 @@ let boudy_main liste flag = if liste = [] && flag = 0 then output_string fic_out
 
 (** Partie écriture **)
 
-let () = output_string fic_out header
+let print_header out = output_string out header
 
-let () = boudy_main [] 0
+let print_ender out = output_string out ender
 
-let () = output_string fic_out ender
+let close_fic out = close_out out
 
-let close = close_out fic_out
+let print_everything modules out =
+	print_header out ;
+	print_nodes_complete modules out ;
+	print_ender out
 
-let _ =
+let compile_tex () =
 	let _ = Sys.command "latex graph.tex" in
 	let _ = Sys.command "dvips graph.dvi" in
-	Sys.command "ps2pdf graph.ps"
+	let _ = Sys.command "ps2pdf graph.ps" in
+	()
 
-
+let build_graph modules =
+  let out = open_fic_out () in
+	print_everything modules out ;
+	close_fic out ;
+	compile_tex ()
+	
+let _ = build_graph ["toto";"tutu";"tyty"]
 
 
 
