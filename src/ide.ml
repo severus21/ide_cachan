1ed2b5860073e4b4fdce50d5d33dd6f6de4f35b8
Alice Rixte <alice.rixte@gmail.com>
2016-11-17 10:40:13 +0100

    commit for merge

--- src/ide.ml
+++ src/ide.ml
@@ -2,9 +2,9 @@ open Tl_ast
 open Plugin_fe_ml
 
 let () =
-  (*let tl = get_ast "src/test_ast.ml" and s = file_to_string "src/test_ast.ml" in 
+ (* let tl = file_to_ast "src/test_ast.ml" and s = file_to_string "src/test_ast.ml" in 
   
   print_ast tl;
-  print_tl_ast (ast_to_tl_ast s tl);*)
+  print_tl_ast (string_to_tl_ast s );*)
   ignore (Plugin_ml.file_extensions);
   unit_tests ()
