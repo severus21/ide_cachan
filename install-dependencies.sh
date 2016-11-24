993652a8cc9294040a41984393c80ee029361141
Laurent Prosperi <laurent.prosperi@ens-cachan.fr>
2016-11-24 13:04:51 +0100

    integration fix

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -9,8 +9,10 @@ TESTING_DEPENDS=""
 #sudo apt-key add - < Release.key
 #sudo echo 'deb http://download.opensuse.org/repositories/home:/ocaml/Debian_7.0/ /' >> /etc/apt/sources.list.d/opam.list
 
-sudo add-apt-repository -y ppa:avsm/ppa
+sudo add-apt-repository --yes ppa:avsm/ppa
 sudo apt-get update -qq
+sudo apt-get install -y opam
+eval $(opam config env)
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra ${LIB_DEPENDS} ${COMPILER_DEPENDS} ${TESTING_DEPENDS}
 
 opam init
