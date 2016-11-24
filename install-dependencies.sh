37cc5fb1368816b52d148baaa1d0ce4a36bc4c4d
Laurent Prosperi <laurent.prosperi@ens-cachan.fr>
2016-11-24 12:48:11 +0100

    integration test

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,9 +5,9 @@ LIB_DEPENDS="texlive-science tex dvips ps2pdf libgtk2.0-dev"
 COMPILER_DEPENDS="make"
 TESTING_DEPENDS=""
 
-wget http://download.opensuse.org/repositories/home:ocaml/Debian_7.0/Release.key
-sudo apt-key add - < Release.key
-sudo echo 'deb http://download.opensuse.org/repositories/home:/ocaml/Debian_7.0/ /' >> /etc/apt/sources.list.d/opam.list
+#wget http://download.opensuse.org/repositories/home:ocaml/Debian_7.0/Release.key
+#sudo apt-key add - < Release.key
+#sudo echo 'deb http://download.opensuse.org/repositories/home:/ocaml/Debian_7.0/ /' >> /etc/apt/sources.list.d/opam.list
 
 sudo apt-get update -qq
 sudo apt-get install -qq opam ocaml ${LIB_DEPENDS} ${COMPILER_DEPENDS} ${TESTING_DEPENDS}
