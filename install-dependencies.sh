d20ea2d282944fee5bcb0250ff1140c1ecf782a4
Laurent Prosperi <laurent.prosperi@ens-cachan.fr>
2016-11-24 12:45:06 +0100

    integration fix

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -7,7 +7,7 @@ TESTING_DEPENDS=""
 
 wget http://download.opensuse.org/repositories/home:ocaml/Debian_7.0/Release.key
 sudo apt-key add - < Release.key
-sudo cho 'deb http://download.opensuse.org/repositories/home:/ocaml/Debian_7.0/ /' >> /etc/apt/sources.list.d/opam.list
+sudo echo 'deb http://download.opensuse.org/repositories/home:/ocaml/Debian_7.0/ /' >> /etc/apt/sources.list.d/opam.list
 
 sudo apt-get update -qq
 sudo apt-get install -qq opam ocaml ${LIB_DEPENDS} ${COMPILER_DEPENDS} ${TESTING_DEPENDS}
