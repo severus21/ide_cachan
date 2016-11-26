e03eff4dd8ec28f955f22bc116ccf2383fd449ed
Laurent Prosperi <laurent.prosperi@ens-cachan.fr>
2016-11-26 14:27:19 +0100

    ...

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 OPAM_DEPENDS="ocamlfind lablgtk ounit ocamlbuild"
-LIB_DEPENDS="texlive-science libgtk2.0-dev libcanberra-gtk-module"
-COMPILER_DEPENDS="make"
+LIB_DEPENDS="ocaml-findlib texlive-science libgtk2.0-dev libcanberra-gtk-module"
+COMPILER_DEPENDS="make ocaml ocaml-native-compilers camlp4-extra opam"
 TESTING_DEPENDS=""
 
 #wget http://download.opensuse.org/repositories/home:ocaml/Debian_7.0/Release.key
@@ -11,17 +11,19 @@ TESTING_DEPENDS=""
 
 sudo add-apt-repository --yes ppa:avsm/ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam ${LIB_DEPENDS} ${COMPILER_DEPENDS} ${TESTING_DEPENDS}
+sudo apt-get install -qq  ${LIB_DEPENDS} ${COMPILER_DEPENDS} ${TESTING_DEPENDS}
 
 export OPAMYES=1
 opam init
 opam switch 4.04.0
 eval `opam config env`
+
 echo "let () =
         try Topdirs.dir_directory (Sys.getenv "OCAML_TOPLEVEL_PATH")
         with Not_found -> ()
   ;;">> ~/.ocamlinit
 
+unset OCAMLLIB
 
 opam install ${OPAM_DEPENDS}
 
