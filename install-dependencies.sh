1cec074874428074d6eb86d25cd7ff3a298f6364
Laurent Prosperi <laurent.prosperi@ens-cachan.fr>
2016-11-26 14:39:00 +0100

    ...

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OPAM_DEPENDS="ocamlfind lablgtk ounit ocamlbuild"
-LIB_DEPENDS="ocaml-findlib texlive-science libgtk2.0-dev libcanberra-gtk-module"
+LIB_DEPENDS="texlive-science libgtk2.0-dev libcanberra-gtk-module"
 COMPILER_DEPENDS="make ocaml ocaml-native-compilers camlp4-extra opam"
 TESTING_DEPENDS=""
 
