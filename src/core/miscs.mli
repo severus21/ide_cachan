4395b9bd818e2a0de83663b01f207e1f3154801e
Laurent Prosperi <laurent.prosperi@ens-cachan.fr>
2016-12-03 15:12:29 +0100

    [plugins/ocaml] tl_ast to core_ast

--- src/core/miscs.mli
+++ src/core/miscs.mli
@@ -1,7 +1,11 @@
+open Gset
+
 (** Define an abstract representation of a file
   - name of structure, header of structure, code of structure, and children 
 * *)
-type c_node = Nil| Node of {name:string;header:string;body:string ref;children:c_ast}
+type c_node = 
+|Nil
+|Node of {name:string;header:string;body:string ref;children:c_ast;meta:gset metaData}
 and c_ast = c_node list
 
 (** To reference a piece of c_ast, we can copy without duplicated part of ast 
@@ -12,5 +16,4 @@ class ptr_ast : c_ast -> object
     method ast : c_ast   
 end                      
 
-val c_ast_to_str : c_ast -> string
 val print_c_ast : c_ast->unit                              
