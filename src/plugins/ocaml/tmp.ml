749ec0ec0ac40d5ddbcb33f1c9a6df441154aff4
Laurent Prosperi <laurent.prosperi@ens-cachan.fr>
2016-12-05 15:45:45 +0100

    [plugins/ocaml] tl_ast to str

--- src/plugins/ocaml/tmp.ml
+++ src/plugins/ocaml/tmp.ml
@@ -1,10 +1,17 @@
-module rec Even : sig 
-            type t = Zero | Succ of Odd.t 
-        end = struct 
-            type t = Zero | Succ of Odd.t 
-        end 
-        and Odd : sig 
-            type t = Succ of Even.t 
-        end = struct 
-            type t = Succ of Even.t 
-        end
+let alpha = 11
+let troll x = x + alpha              
+
+class ptr_ast x: object 
+    val p_ast : c_ast ref 
+    method ast : c_ast 
+end = object 
+    val p_ast = ref Nil 
+    method ast = Nil 
+end
+
+module OrderList (T:Comparable) = struct 
+        exception Empty 
+        type content = T.t 
+        type t = content list ref 
+        let comp = T.compare 
+    end
