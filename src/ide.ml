dc8a8e998f707f51decc1727ce505dd19408d59b
Nounours <le-run@crans.org>
2016-12-13 17:50:45 +0100

    Now plugins are properly compiled and loaded

--- src/ide.ml
+++ src/ide.ml
@@ -4,6 +4,9 @@ open Plugins.Factory
 
 let () =
     load_plugins ();
-    Printf.printf "%s\n" "*********************** IDE ***********************";
+    List.iter
+        (fun plugin -> Printf.printf "%s plugin loaded\n%!" plugin#name)
+        (get_plugins ());
+    Printf.printf "%s\n" "*********************** IDE ***********************%!";
     main ()
 (*let () = build_graph []*)
