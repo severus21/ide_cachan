4ea9413fb889c566d813c6f73675dff64f10bc7f
Kevin Le run <le-run@crans.org>
2017-01-05 11:46:56 +0100

    Export works

---
+++ sample_projects/ocaml/main.ml
@@ -0,0 +1,11 @@
+
+module Test = struct
+    let test = "Hello, world!"
+end
+
+let toto x = x
+;;
+
+let () =
+    Printf.printf "%s\n" Test.test
+;;
--- src/gui/guiMain.ml
+++ src/gui/guiMain.ml
@@ -53,44 +53,84 @@ let main () =
     let factory = new GMenu.factory edit_menu ~accel_group in
     ignore(factory#add_item "Find" ~key:_F
         ~callback: (fun () -> ignore(GuiFindDialog.find_dialog window test_set)));
+    ignore(factory#add_item "Dependencies" ~key:_D ~callback:notimp_callback);
 
     (* Navlist *)
     let navlist = new GuiNavlist.navlist ~packing:vbox#add in
 
-    (* Import callback *)
-    (* This function opens a folder chooser dialog and imports the
-     * project there *)
-    let import_callback () : unit =
+    (* Currently opened folder containing the project *)
+    let current_folder : string option ref = ref None in
+    (* Currently opened project as c_ast *)
+    let current_root : Core.Miscs.c_ast ref = ref [] in
+    (* Opens a folder chooser dialog and applies the continuation k to the
+     * path if it is selected *)
+    let get_folder (k:string -> unit) : unit =
         let file_chooser =
             GWindow.file_chooser_dialog ~parent:window
                                         ~action:`SELECT_FOLDER
                                         ~show:true () in
         file_chooser#add_select_button_stock `CANCEL `CANCEL;
         file_chooser#add_select_button "Open" `SELECT_FOLDER;
-        let _import () : unit =
-            let path = match file_chooser#filename with
-            | Some p -> p
-            | None -> "No file"
-            in
-            Printf.printf "Loading with first plugin at %s\n%!" path;
-            let plugin = List.hd (Plugins.Factory.get_plugins ()) in
-            let ast = plugin#path_to_c_ast path in
-            Core.Miscs.print_c_ast (fst ast);
-            navlist#set_root (fst ast)
+        let apply () : unit =
+            match file_chooser#filename with
+            | Some path -> k path
+            | None -> Printf.printf "No file\n"
         in
         begin
             match file_chooser#run () with
-            | `SELECT_FOLDER -> _import ()
+            | `SELECT_FOLDER -> apply ()
             | `CANCEL -> ()
             | _ -> ()
         end;
         file_chooser#destroy ()
     in
 
+    (* Import callback *)
+    (* This function opens a folder chooser dialog and imports the
+     * project there *)
+    let import_callback () : unit =
+        let _import (path:string) : unit =
+            current_folder := Some path;
+            Printf.printf "Loading with first plugin at %s\n%!" path;
+            let plugin = List.hd (Plugins.Factory.get_plugins ()) in
+            let ast = plugin#path_to_c_ast path in
+            Core.Miscs.print_c_ast (fst ast);
+            navlist#set_root (fst ast);
+            current_root := fst ast
+        in
+        get_folder _import
+    in
+
+    (* Export As callback *)
+    (* This function opens a folder chooser dialog and exports the
+     * project there *)
+    let export_as_callback () : unit =
+        let _export (path:string) : unit =
+            current_folder := Some path;
+            Printf.printf "Exporting to %s with first plugin\n%!" path;
+            let plugin = List.hd (Plugins.Factory.get_plugins ()) in
+            plugin#c_ast_to_folder path !current_root
+        in
+        get_folder _export
+    in
+
+    (* Export callback *)
+    (* This function exports the project into the current folder *)
+    let export_callback () : unit =
+        match !current_folder with
+        | None -> export_as_callback ()
+        | Some path ->
+                begin
+                    let plugin = List.hd (Plugins.Factory.get_plugins ()) in
+                    plugin#c_ast_to_folder path !current_root
+                end
+    in
+
     (* File menu *)
     let factory = new GMenu.factory file_menu ~accel_group in
     ignore(factory#add_item "Import" ~key:_I ~callback: import_callback);
-    ignore(factory#add_item "Export" ~key:_E ~callback: notimp_callback);
+    ignore(factory#add_item "Export As" ~key:_E ~callback: export_as_callback);
+    ignore(factory#add_item "Export" ~key:_S ~callback: export_callback);
     ignore(factory#add_item "Quit" ~key:_Q
         ~callback: (fun () -> if confirm_quit () then Main.quit ()));
 
