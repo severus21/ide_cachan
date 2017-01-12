0db60a4b596fab061fb0119be78de83884eab946
Nounours <le-run@crans.org>
2017-01-12 09:18:23 +0100

    [minor] fixed printf in main

--- src/ide.ml
+++ src/ide.ml
@@ -2,10 +2,10 @@ open Gui.GuiMain
 open Plugins.Factory
 
 let () =
+    Printf.printf "%s\n%!" "*********************** IDE ***********************";
     load_plugins ();
     List.iter
         (fun plugin -> Printf.printf "%s plugin loaded\n%!" plugin#name)
         (get_plugins ());
-    Printf.printf "%s\n" "*********************** IDE ***********************%!";
     main ()
 (*let () = build_graph []*)
